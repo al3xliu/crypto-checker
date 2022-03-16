6fc3f77e45f550f51fb39a648234b4fd52142d23
Alex Liu <alexliusch@gmail.com>
2022-03-15 20:02:29 -0400

    add precondition

---
+++ tests/general/RequireUniqueTest.java
@@ -0,0 +1,13 @@
+package hardwarebacked;
+
+import org.checkerframework.checker.crypto.qual.RequireUnique;
+import org.checkerframework.checker.crypto.qual.Unique;
+
+class EnsureUniqueTest {
+    @EnsureUnique(value = "#1", whatever = "algo1")
+    void test1(@Unique({"algo1"}) String x, @Unique({"algo1"}) String y) {}
+
+    @EnsureUnique(value = "#1", whatever = "algo2")
+        // :: error: contracts.postcondition
+    void test2(@Unique({"algo1"}) String x, @Unique({"algo1"}) String y) {}
+}
